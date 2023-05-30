1716a016824112e0dec863109cf9c0b05bf8ac48
AF332 <134966071+AF332@users.noreply.github.com>
2023-05-30 17:19:17 +0100

    Add files via upload

--- Abbott.py
+++ Abbott.py
@@ -34,11 +34,37 @@ print(Sell) # Sell list is printed
 # Data frame is then plotted into a graph
 plt.figure(figsize = (12,5))
 plt.plot(dataset_2019['Adj Close'], label = 'Stock Price', color = 'blue', alpha = 0.5)
+plt.plot(dataset_2019['MA20'], label = 'MA20', color = 'k', alpha = 0.5)
+plt.plot(dataset_2019['MA50'], label = 'MA50', color = 'magenta', alpha = 0.5)
+plt.scatter(dataset_2019.iloc[Buy].index, dataset_2019.iloc[Buy]['Adj Close'], marker = '^', color = 'g', s = 100)
+plt.scatter(dataset_2019.iloc[Sell].index, dataset_2019.iloc[Sell]['Adj Close'], marker = 'v', color = 'r', s = 100)
 plt.legend()
 plt.xlabel('Date')
 plt.ylabel('Price in Pounds')
 plt.title('Abbott')
+plt.show()
 
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = 120))
 plt.gca().xaxis.set_tick_params(rotation = 30)
-plt.show()
+
+dataset2 = pd.read_csv(r'E:\3rd Year\Semester 1\Research and Development Skills\Project\J&J stock data.csv', index_col='Date', parse_dates=True)
+print(dataset2)
+
+dataset2['MA20'] = dataset2['Adj Close'].rolling(20).mean()
+dataset2['MA50'] = dataset2['Adj Close'].rolling(50).mean()
+print(dataset2)
+
+dataset2_2019 = dataset2.loc['2019-08-01':'2022-08-01']
+Buy2 = []
+Sell2 = []
+
+for j in range (len(dataset2_2019)):
+    if dataset2_2019.MA20.iloc[j] > dataset2_2019.MA50.iloc[j] \
+    and dataset2_2019.MA20.iloc[j-1] < dataset2_2019.MA50.iloc[j-1]:
+        Buy.append(j)
+    elif dataset2_2019.MA20.iloc[j] < dataset2_2019.MA50.iloc[j] \
+    and dataset2_2019.MA20.iloc[j-1] > dataset2_2019.MA50.iloc[j-1]:
+        Sell.append(j)
+
+print(Buy)
+Print(Sell)
